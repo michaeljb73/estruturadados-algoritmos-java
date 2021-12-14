8b7c22c4b916d85075e2141b6b01a4fd6acd6eed
Michael J. Bezerra <michaeljb73@gmail.com>
2021-12-13 23:16:39 -0300

    Aula 03 de Vetor. Criando método "adicionar".

--- src/main/java/com/estruturadados/vetor/Vetor.java
+++ src/main/java/com/estruturadados/vetor/Vetor.java
@@ -2,8 +2,39 @@ package com.estruturadados.vetor;
 
 public class Vetor {
     private String[] elementos;
+    private int tamanho;
 
     public Vetor(int capacidade) {
         this.elementos = new String[capacidade];
+        this.tamanho = 0;
+    }
+
+/*     public void adiciona(String elemento) {
+        for(int i = 0; i < elementos.length ; i++) {
+            if(this.elementos[i] == null) {
+                this.elementos[i] = elemento;
+                break;
+            }
+        }
+    } */ // Primeiro método - ineficiente
+
+    /* public void adiciona(String elemento) throws Exception {
+        if(this.tamanho < elementos.length) {
+            this.elementos[this.tamanho] = elemento;
+            this.tamanho++;
+        } else {
+            throw new Exception("Vetor já está cheio, não é possível adicionar mais elementos !");
+        }
+    } */ // Segundo método
+
+    public boolean adiciona(String elemento) {
+        if(this.tamanho < elementos.length) {
+            this.elementos[this.tamanho] = elemento;
+            this.tamanho++;
+            System.out.println("O elemento \"" + elemento + "\" foi adicionado com sucesso !");
+            return true;
+        }
+        System.out.println("O \"" + elemento + "\" não pode ser adicionado !");
+        return false;
     }
 }
---
+++ src/main/java/com/estruturadados/vetor/teste/Aula03.java
@@ -0,0 +1,21 @@
+package com.estruturadados.vetor.teste;
+
+import com.estruturadados.vetor.Vetor;
+
+public class Aula03 {
+    public static void main(String[] args) {
+        Vetor vetor = new Vetor(2);
+
+        /* try {
+            vetor.adiciona("elemento 1");
+            vetor.adiciona("elemento 2");
+            vetor.adiciona("elemento 3");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } */
+
+        vetor.adiciona("elemento 1");
+        vetor.adiciona("elemento 2");
+        vetor.adiciona("elemento 3");
+    }
+}
